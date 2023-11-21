0dfdb5b0cf3882818e7b509f6f0fd56cba9a9184
Sirojiddin07 <khamidovsirojiddin998@gmail.com>
2023-11-21 11:23:11 +0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {OurServicesComponent} from "./routes/our-services/our-services.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'services', pathMatch: "full" },
+  { path: 'services', component: OurServicesComponent }
+];
---
+++ src/app/routes/our-services/our-services.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-our-services',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './our-services.component.html',
+  styleUrls: ['./our-services.component.scss']
+})
+export class OurServicesComponent {
+
+}
